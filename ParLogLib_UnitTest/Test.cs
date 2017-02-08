bb0df22cbd44a45a967955325ce5dcdfe052d8b3
johannes fleck <johannes.fleck@gmail.com>
2017-02-08 21:14:15 +0100

    added unit test project

---
+++ ParLogLib_UnitTest/Test.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace ParLogLib_UnitTest
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+            Assert.AreEqual(1, 2);
+        }
+    }
+}
+
